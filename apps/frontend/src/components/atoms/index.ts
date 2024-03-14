5b806f97183ed6dfe883e59eee921805f3319ff2
Romain Petit <romain.petit@vedana.com>
2024-03-14 17:14:01 +0100

    style(frontend): Textarea component implementation

--- apps/frontend/src/components/atoms/index.ts
+++ apps/frontend/src/components/atoms/index.ts
@@ -34,5 +34,6 @@ export * from "./slider";
 export * from "./switch";
 export * from "./table";
 export * from "./tabs";
+export * from "./textarea";
 export * from "./toast";
 export * from "./tooltip";
---
+++ apps/frontend/src/components/atoms/textarea.tsx
@@ -0,0 +1,23 @@
+import React, { forwardRef } from "react";
+
+import { classNames } from "@/libraries/classnames";
+
+import type { TextareaHTMLAttributes } from "react";
+
+export interface TextareaProps
+  extends TextareaHTMLAttributes<HTMLTextAreaElement> {}
+
+export const Textarea = forwardRef<HTMLTextAreaElement, TextareaProps>(
+  ({ className, ...props }, ref) => {
+    return (
+      <textarea
+        ref={ref}
+        className={classNames(
+          "flex min-h-[60px] w-full rounded-md border border-input bg-transparent px-3 py-2 text-sm shadow-sm placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50",
+          className,
+        )}
+        {...props}
+      />
+    );
+  },
+);
