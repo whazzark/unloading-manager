8932e3561b38f50eb2c91a71b12457bbd6b0cea4
Romain Petit <romain.petit@vedana.com>
2024-03-14 17:07:06 +0100

    style(frontend): Badge component implementation

--- apps/frontend/src/components/atoms/index.ts
+++ apps/frontend/src/components/atoms/index.ts
@@ -3,6 +3,7 @@ export * from "./alert";
 export * from "./alert-dialog";
 export * from "./aspect-ratio";
 export * from "./avatar";
+export * from "./badge";
 export * from "./button";
 export * from "./card";
 export * from "./checkbox";
